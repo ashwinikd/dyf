f47bd47630c6a32e78f5cd91a965a0b54af78fe4
Ashwini Dhekane <ashwinidhekane@Ashwinis-MacBook-Pro.local>
2013-02-07 03:42:58 +0530

    Improved privacy policy

--- app/views/privacy.php
+++ app/views/privacy.php
@@ -4,6 +4,20 @@
 <p>
 	We take your privacy seriously. We will only store information which is required for successful functioning of the application. We also provide you with an option of deleting the data which was captured through your Facebook account.
 </p>
+
+<h4>Luvanonymous is</h4>
+<ul>
+	<li>A platform which helps users identify prospective dates.</li>
+	<li>Notifies the concerned users when it detects mutual interests between two users. Luvanonymous helps you figure out which of your interests might be interested in dating you. It is upto users to take on the conversion from there onwards.</li>
+</ul>
+
+<h4>Lovanonymous is not</h4>
+<ul>
+	<li>A recommendation engine. Our suggestions are purely based on choices made by users. We do not predict prospective dates on basis of your profile information or activity on Facebook.</li>
+	<li>Providing any information on the users who are interested in you. It will notify you only if there is a mutual interest.</li>
+	<li>Making your friend selections public. What happens at Luvanonymous stays in Lovanonymous.</li>
+</ul>
+
 <h4>We store</h4>
 <ul>
 	<li>Your name and gender.</li>
@@ -31,5 +45,9 @@
 	<li>List of your crushes who have crush on you.</li>
 </ul>
 <p>Note that your crush will still be able to see you in "Who wants to date you?" section if s/he has added you as his/her crush and has not yet chosen to delete the data from our system.</p> <p>If you revisit the App after deleting your data we will be storing your name, gender and name&gender of your friends.</p>
+
+
+<h4>DISCLAIMER</h4>
+<p>YOUR USE OF LUVANONYMOUS IS AT YOUR SOLE RISK. LUVANONYMOUS IS PROVIDED ON AN "AS IS" AND "AS AVAILABLE" BASIS. WE RESERVE THE RIGHT TO RESTRICT OR TERMINATE YOUR ACCESS TO LUVANONYMOUS OR ANY FEATURE OR PART THEREOF AT ANY TIME. LUVANONYMOUS EXPRESSLY DISCLAIMS ALL WARRANTIES OF ANY KIND, WHETHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE IMPLIED WARRANTIES THAT THE LUVANONYMOUS OR THE SERVICE CONTENT IS NONINFRINGING, AS WELL AS WARRANTIES IMPLIED FROM A COURSE OF PERFORMANCE; THAT ACCESS TO LUVANONYMOUS WILL BE UNINTERRUPTED OR ERROR-FREE; THAT LUVANONYMOUS WILL BE SECURE; THAT LUVANONYMOUS OR THE SERVER THAT MAKES LUVANONYMOUS AVAILABLE WILL BE VIRUS-FREE; OR THAT INFORMATION ON LUVANONYMOUS WILL BE COMPLETE, ACCURATE OR TIMELY.</p>
 </div>
 <?php $this->showView("footer")?>
