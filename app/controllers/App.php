1d1183909aa86834c38f7b1ac253cfc6ae39f6f6
Ashwini Dhekane <ashwinidhekane@Ashwinis-MacBook-Pro.local>
2013-02-10 00:53:33 +0530

    changed notifications

--- app/controllers/App.php
+++ app/controllers/App.php
@@ -126,14 +126,14 @@ class AppController extends Controller {
 			$frndInterests = self::$db->getInterests($id);
 			$return[]     = $this->friends["idMap"][$id];
 			
-			$this->sendFbNotification($id, "Somebody is interested in dating you. Check out who, on Luvanonymous!");
+			$this->sendFbNotification($id, "Somebody in your friend list is interested in dating you. Check out who, on Luvanonymous!");
 			
 			if(in_array($this->userId, $frndInterests)) {
 				self::$db->addMatch($this->userId, $this->friends["idMap"][$id]);
 				self::$db->addMatch($id, array("uid" => $this->userProfile["id"], "name" => $this->userProfile["name"], "sex" => $this->userProfile["gender"]));
 				
-				$this->sendFbNotification($id, "Somebody is interested in dating you. Check out who, on Luvanonymous!");
-				$this->sendFbNotification($this->userId, "Somebody is interested in dating you. Check out who, on Luvanonymous!");
+				$this->sendFbNotification($id, "Guess what?! We found a match for you. Love on and spread the word!!");
+				$this->sendFbNotification($this->userId, "Guess what?! We found a match for you. Love on and spread the word!!");
 			}
 		}
 		
